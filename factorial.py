a3df6965723c45a54318925438c77f28554e4355
Anirban Kar <anirban.k59@gmail.com>
2023-08-16 20:09:36 +0530

    add message

--- factorial.py
+++ factorial.py
@@ -1,12 +1,12 @@
 def factorial(number):
     if number == 0:
         return 1
-    elif number < 0:
+    if number < 0:
         return None
-    else:
-        factr = 1
-        for item in range(1,number + 1):
-            factr *= item
+
+    factr = 1
+    for item in range(1,number + 1):
+        factr *= item
     return factr
 
 assert factorial(5) == 120
@@ -14,3 +14,4 @@ assert factorial(0) == 1
 assert factorial(10) == 3628800
 assert factorial(1) == 1
 assert factorial(-1) == None
+print("All tests passed!")
