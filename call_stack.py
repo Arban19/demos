7a8b56925e5e06d53ef0592b6493718a638c3b87
Anirban Kar <anirban.k59@gmail.com>
2023-08-16 23:11:36 +0530

    improve code

--- call_stack.py
+++ call_stack.py
@@ -1,21 +1,20 @@
-def next_digit(digits):
-    return append(digits)
+def next_digit(number):
+    return append(number)
 
 def increment(number):
     return number + 1
 
 def append(number):
     last_digit_int = int(number[-1])
-    return str(number) + str(increment(last_digit_int))
+    if last_digit_int in range(0,9):
+        appended_number = str(number) + str(increment(last_digit_int))
+        return appended_number
+    elif last_digit_int == 9:
+        appended_number = str(number) + "0"
+        return appended_number
 
 assert next_digit("6") == "67"
 assert next_digit("12") == "123"
 assert next_digit("713") == "7134"
-# assert next_digit("9") == "90"
+assert next_digit("9") == "90"
 print("All tests passed!")
-
-# subproblems
-# - increment a number
-# - preserve the entire input string
-# - get last digit from the input string
-# - concat input string with incremented
