b0d7ac99f31a85f12d3c94582720428fd1d13a52
Anirban Kar <anirban.k59@gmail.com>
2023-08-23 11:33:38 +0530

    add exception handling in multiplication table program

--- multiplication_table.py
+++ multiplication_table.py
@@ -1,5 +1,8 @@
-x = int(input("Enter the number: "))
+x = (input("Enter the number: "))
 
-print(f"The multiplication table of {x} is:")
-for i in range(1,21):
-    print(f"{x}*{i} = {x*i}")
+try:
+    print(f"The multiplication table of {x} is:")
+    for i in range(1,21):
+        print(f"{int(x)}*{i} = {int(x)*i}")
+except ValueError:
+    print("Invalid Input")
