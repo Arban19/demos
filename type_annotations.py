0a5ab4b7a880706e2f30dabb69bb3870856b00df
Anirban Kar <anirban.k59@gmail.com>
2024-01-07 18:02:41 +0530

    type annotations

---
+++ type_annotations.py
@@ -0,0 +1,20 @@
+def add(x: int,y: int):
+    return x + y
+
+print(add(3,7))
+print(add("cat","dog"))
+
+x = add(54,15)
+
+y = [1,2,3,4,5]
+
+for n in y:
+    z = n * 5
+    print(z)
+
+print(z)
+
+a: str  = "Duke"
+print(a.istitle())
+
+
