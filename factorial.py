b6f4954eaf6c8fafbdd23234181242e3479f65ec
Anirban Kar <anirban.k59@gmail.com>
2024-01-31 19:24:32 +0530

    add recursion in factorial

--- factorial.py
+++ factorial.py
@@ -9,9 +9,18 @@ def factorial(number):
         factr *= item
     return factr
 
-assert factorial(5) == 120
-assert factorial(0) == 1
-assert factorial(10) == 3628800
-assert factorial(1) == 1
-assert factorial(-1) == None
-print("All tests passed!")
+def factorial(number):
+    if number < 0:
+        return None
+    if number == 0:
+        return 1
+    result = number*factorial(number -1)
+    return result
+
+
+# assert factorial(5) == 120
+# assert factorial(0) == 1
+# assert factorial(10) == 3628800
+# assert factorial(1) == 1
+# assert factorial(-1) == None
+# print("All tests passed!")
